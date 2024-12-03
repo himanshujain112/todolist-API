06748e5bf008447484df9893b722ff73dfb81370
Himanshu Jain <56267719+himanshujain112@users.noreply.github.com>
2024-12-03 20:24:08 +0530

    CRUD Operations working correctly !

--- App/controller/todo_controller.py
+++ App/controller/todo_controller.py
@@ -1,49 +1,40 @@
 from flask import request, Blueprint
 from flask_restful import Resource, Api
-
+from model.todo_model import TodoModel
 todos = {'1': 'Go to Market', '2': 'Buy a car', '3': 'Learn Python'}
+
+obj = TodoModel()
 
 class TodoResource(Resource):
 #Print todo
     def get(self, todo_id=None):
         if todo_id:
-            if todo_id in todos:
-                return {todo_id: todos[todo_id]}, 200
-            else:
-                return {"error": "Todo not found"}, 404
-        return todos, 200
+            return obj.get_todo_by_id(todo_id)
+        else:
+            return obj.get_all_todos()
 
 #Create new Todo
     def post(self):
         data = request.get_json()
-        if not data or "id" not in data or "todo" not in data:
+        if not data:
             return {"error": "Invalid data"}, 400
-        todo_id = data["id"]
-        todo_text = data["todo"]
-
-        if todo_id in todos:
-            return {"error": "Todo id already present!"}, 400
-        todos[todo_id] = todo_text
-        return {"message" : "Todo Created successfully!" , "todo": {todo_id: todo_text}}, 201
-
+        return obj.create_new_todo(data["todo"])
+    
 #Update a Todo
     def put(self, todo_id):
-        if todo_id not in todos:
-            return {"error": "Todo not found"}, 404
+        #if todo_id not in todos:
+         #   return {"error": "Todo not found"}, 404
         data = request.get_json()
+        return obj.update_todo_by_id(todo_id, data["todo"])
 
-        if not data or "todo" not in data:
-            return {"error": "Invalid data"}, 400
-        todos[todo_id] = data["todo"]
-        return {"message" : "Todo Updated successfully!" , "todo": {todo_id: todos[todo_id]}}, 200
-    
+
+        
 #Delete a Todo
     def delete(self, todo_id):
-        if todo_id not in todos:
-            return {"error": "Todo not found"}, 404
-        del todos[todo_id]
-        return {"message" : "Todo Deleted successfully!"}, 200
+        #if todo_id not in todos:
+         #   return {"error": "Todo not found"}, 404
+        return obj.delete_todo_by_id(todo_id)
 
 todoBP = Blueprint('himan', __name__)
 todoAPI = Api(todoBP)
-todoAPI.add_resource(TodoResource, '/todo', '/todo/<string:todo_id>')
+todoAPI.add_resource(TodoResource, '/todo', '/todo/<int:todo_id>')
---
+++ App/model/todo_model.py
@@ -0,0 +1,71 @@
+import sqlite3
+from flask import jsonify
+class TodoModel():
+
+    def __init__(self):
+        print("Connecting to database...")
+        try:
+            self.conn = sqlite3.connect('todo.db', check_same_thread=False)
+            self.cursor = self.conn.cursor()
+            self.cursor.execute("create table if not exists todos (id INTEGER PRIMARY KEY AUTOINCREMENT, todo TEXT)")
+            print("Database connected successfully!")
+            #self.cursor.execute("insert into todos (todo) values ('Go to Market')")
+            #self.conn.commit()
+        except Exception as e:
+            print("Error in connecting to database: ", e)
+    
+    def get_all_todos(self):
+        try:
+            self.cursor.execute("select * from todos")
+            result = self.cursor.fetchall()
+            if not result:
+                return jsonify("No Todos Found!")
+            return jsonify([{"Todo" : row[1]} for row in result])
+        except Exception as e:
+            return "Error in fetching Todos!"
+    
+    def get_todo_by_id(self, todo_id):
+        try:
+            if not todo_id:
+                return "Invalid Id!"
+            todo_id = int(todo_id)
+            self.cursor.execute("select * from todos where id=?", (todo_id,))
+            result = self.cursor.fetchone()
+            return jsonify({'Todo' : result[1]})
+        except Exception as e:
+            return "Todo Not Found!"
+    
+    def create_new_todo(self, todo):
+        try:
+            self.cursor.execute("insert into todos (todo) values (?)", (todo,))
+            self.conn.commit()
+            return "Todo Created successfully!"
+        except Exception as e:
+            return "Error in creating Todo!"
+
+    def update_todo_by_id(self, todo_id, todo):
+        try:
+            self.cursor.execute("SELECT * FROM todos WHERE id=?", (todo_id,))
+            result = self.cursor.fetchone()
+            if result is None:
+                return jsonify({"message": "Todo Not Found!"})
+            #todo_id = int(todo_id)
+            self.cursor.execute("update todos set todo=? where id=?", (todo, todo_id))
+            self.conn.commit()
+            return "Todo Updated successfully!"
+        except Exception as e:
+            return "Todo Not Found!"
+    
+    def delete_todo_by_id(self, todo_id):
+        try:
+            self.cursor.execute("SELECT * FROM todos WHERE id=?", (todo_id,))
+            result = self.cursor.fetchone()
+            if result is None:
+                return jsonify({"message": "Todo Not Found!"})
+            todo_id = int(todo_id)
+            self.cursor.execute("delete from todos where id=?", (todo_id,))
+            self.conn.commit()
+            return "Todo Deleted successfully!"
+        except Exception as e:
+            return "Todo Not Found!"
+    
